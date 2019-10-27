2f10d9b723b3e901843010a95c246068c3300799
Nastya <nastya_mi@icloud.com>
2019-10-27 23:24:13 +0300

    feat: add new functional to switcher

--- js/main.js
+++ js/main.js
@@ -1,8 +1,26 @@
-var canvas = document.querySelector("canvas").getContext("2d");
-
-var img = document.createElement('img');
-img.src = './data/image.png';
-img.addEventListener("load", function() {
-  canvas.drawImage(img,20,10,256,128);
+window.addEventListener('load', function() {
+  document.querySelector('.yellow-pic').addEventListener('change', drawYellowPicture);
+  document.querySelector('.red-pic').addEventListener('change', drawRedPicture);
+  document.querySelector('.rs-logo').addEventListener('change', drawRSS);
+  document.querySelector('rs-logo').dispatchEvent(new Event('change'));
 });
+
+let canvas = document.querySelector('canvas');
+let context = canvas.getContext('2d');
+
+function drawRSS() {
+  let img = new Image();
+  img.onload = function() {
+    context.drawImage(img, 20, 10, 256, 128);
+  }
+  img.src = './data/image.png';
+};
+
+function drawYellowPicture() {
+
+}
+
+function drawRedPicture() {
+
+}
 
